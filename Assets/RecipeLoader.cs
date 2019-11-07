bb7a8c1f065df59c3b8281214a7400b21644311d
zjohnson5455 <zacharyjohnsonri@gmail.com>
2019-11-07 18:20:08 -0500

    Can switch between scenes when you hit the bumper button

---
+++ Assets/RecipeLoader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.MagicLeap;
+
+public class RecipeLoader : MonoBehaviour
+{
+    
+    #region Private Variables
+    private ControllerConnectionHandler _controllerConnectionHandler;
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controllerConnectionHandler = GetComponent<ControllerConnectionHandler>();
+
+        MLInput.OnControllerButtonUp += HandleOnButtonUp;
+        MLInput.OnControllerButtonDown += HandleOnButtonDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnDestroy()
+    {
+        MLInput.OnControllerButtonDown -= HandleOnButtonDown;
+        MLInput.OnControllerButtonUp -= HandleOnButtonUp;
+    }
+    
+    private void HandleOnButtonDown(byte controllerId, MLInputControllerButton button)
+    {
+        MLInputController controller = _controllerConnectionHandler.ConnectedController;
+        if (controller != null && controller.Id == controllerId &&
+            button == MLInputControllerButton.Bumper)
+        {
+            Debug.Log("Button down");
+        }
+    }
+
+    /// <summary>
+    /// Handles the event for button up.
+    /// </summary>
+    /// <param name="controller_id">The id of the controller.</param>
+    /// <param name="button">The button that is being released.</param>
+    private void HandleOnButtonUp(byte controllerId, MLInputControllerButton button)
+    {
+        MLInputController controller = _controllerConnectionHandler.ConnectedController;
+        if (controller != null && controller.Id == controllerId &&
+            button == MLInputControllerButton.Bumper)
+        {
+            Debug.Log("Button up");
+            SceneManager.LoadScene("Recipe Information");
+        }
+    }
+}
+
