95f0b7094a5850613d973e1e50705215da02dc97
danielle-fang <25496850+danielle-fang@users.noreply.github.com>
2019-11-10 19:00:10 -0500

    working on timers

---
+++ Assets/MagicLeap/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+public class Timer : MonoBehaviour
+{
+
+    public int Time; //Seconds Overall
+    public Text Timer_disp; //UI Text Object
+
+    // A constructor that takes parameter to set the properties
+    public Timer(int given_time)
+    {
+        Time = given_time;
+    }
+
+    void Start()
+    {
+        StartCoroutine("LoseTime");
+        Time.timeScale = 1; //Just making sure that the timeScale is right
+    }
+    void Update()
+    {
+        Timer_disp.text = ("" + Time); //Showing the Score on the Canvas
+    }
+    //Simple Coroutine
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            Time--;
+        }
+    }
+
+}
+
+
---
+++ Assets/MagicLeap/Scripts/Timer_v1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TimerExample : MonoBehaviour
+{
+    string s;
+
+    void Update()
+    {
+        s = System.TimeSpan.FromSeconds((int)Time.timeSinceLevelLoad).ToString();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(s);
+    }
+}
