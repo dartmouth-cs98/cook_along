62dedc69284c58906994d1edca059bd0ef2d915b
zjohnson5455 <zacharyjohnsonri@gmail.com>
2019-11-11 19:09:48 -0500

    I think swiping through recipes works. Not able to test with device at the moment

--- Assets/RecipeLoader.cs
+++ Assets/RecipeLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.MagicLeap;
 
@@ -9,6 +10,9 @@ public class RecipeLoader : MonoBehaviour
     
     #region Private Variables
     private ControllerConnectionHandler _controllerConnectionHandler;
+    private GameObject _recipe1;
+    private bool _recipe1Active;
+    private GameObject _recipe2;
     #endregion
     
     // Start is called before the first frame update
@@ -16,14 +20,35 @@ public class RecipeLoader : MonoBehaviour
     {
         _controllerConnectionHandler = GetComponent<ControllerConnectionHandler>();
 
+        _recipe1 = GameObject.Find("Viewport/Recipe 1");
+        _recipe2 = GameObject.Find("Viewport/Recipe 2");
+        _recipe1Active = true;
+
         MLInput.OnControllerButtonUp += HandleOnButtonUp;
         MLInput.OnControllerButtonDown += HandleOnButtonDown;
+        
+        // used for reference: https://github.com/larkintuckerllc/magic-leap-patterns/blob/master/Assets/MagicLeap/Examples/Scripts/MeshingExample.cs
+        MLInput.OnControllerTouchpadGestureStart += OnTouchpadGestureStart;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateActiveRecipe();
+    }
+
+    void updateActiveRecipe()
+    {
+        if (_recipe1Active)
+        {
+            _recipe1.GetComponent<Image>().color = new Color(0, 213, 0);
+            _recipe2.GetComponent<Image>().color = new Color(245, 223, 186);
+        }
+        else
+        {
+            _recipe2.GetComponent<Image>().color = new Color(0, 213, 0);
+            _recipe1.GetComponent<Image>().color = new Color(245, 223, 186);
+        }
     }
     
     void OnDestroy()
@@ -56,6 +81,23 @@ public class RecipeLoader : MonoBehaviour
             Debug.Log("Button up");
             SceneManager.LoadScene("Recipe Information");
         }
+    }
+    
+    private void OnTouchpadGestureStart(byte controller_id, MLInputControllerTouchpadGesture gesture)
+    {
+        if (gesture.Type == MLInputControllerTouchpadGestureType.Swipe
+            && gesture.Direction == MLInputControllerTouchpadGestureDirection.Up)
+        {
+            _recipe1Active = !_recipe1Active;
+        }
+        
+        if (gesture.Type == MLInputControllerTouchpadGestureType.Swipe
+            && gesture.Direction == MLInputControllerTouchpadGestureDirection.Down)
+        {
+            _recipe1Active = !_recipe1Active;
+        }
+        
+        
     }
 }
 
