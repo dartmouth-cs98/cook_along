5012b4ac969fb51f73ab7a2036120b5412c9de80
Erika Ogino <erika.ogino.20@dartmouth.edu>
2019-11-13 15:23:04 -0500

    Text UI updates based on input

---
+++ Assets/Scenes/DynamicText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicText : MonoBehaviour
+{
+	private Text thisText;
+	private int step_number;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	thisText = GetComponent<Text>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKeyDown(KeyCode.P))
+        {
+            step_number +=1;
+        }
+        thisText.text = "Step " + step_number;
+    }
+}
