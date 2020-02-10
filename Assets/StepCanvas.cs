0d5b06cb78c2b03ba445ec27fcd034fc0886d459
Erika Ogino <erika.ogino.20@dartmouth.edu>
2020-02-09 22:34:39 -0500

    images URL is working but need to figure out how to delete all old components

--- Assets/StepCanvas.cs
+++ Assets/StepCanvas.cs
@@ -10,6 +10,7 @@ using System.Diagnostics;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.MagicLeap;
 using System.IO;
+using System.Text;
 
 
 public class StepCanvas : MonoBehaviour
@@ -49,6 +50,7 @@ public class StepCanvas : MonoBehaviour
     private int seconds;
     private String niceTime; 
     private bool called = true;
+    private bool firstUpdate= true; 
  
     // Start is called before the first frame update
     void Start()
@@ -58,7 +60,10 @@ public class StepCanvas : MonoBehaviour
         xCoord= 210;
         UnityEngine.Debug.Log("Before URL List");
         URLsList= RecipeInfo.ingredientURLlistoflist;
+        UnityEngine.Debug.Log("After URL List");
+        UnityEngine.Debug.Log(URLsList);
         URLs=URLsList[0];
+        UnityEngine.Debug.Log("current step URLS" + URLs);
         
         
         
@@ -81,6 +86,7 @@ public class StepCanvas : MonoBehaviour
         countdown = GameObject.Find("Timer").GetComponent<Text>();
         UnityEngine.Debug.Log(countdown);
         timeLeft = (float)(-1);
+        canvas = GameObject.Find("Canvas");
     }
     
     void Update()
@@ -106,10 +112,33 @@ public class StepCanvas : MonoBehaviour
         
     //UnityEngine.Debug.Log("Before First If Statement");
     if(GetOkay() && RecipeInfo.RecipeVar != null && step_number < (RecipeInfo.RecipeVar.steps.Count - 1)) {
+           UnityEngine.Debug.Log("Inside first if statement");
+           // UnityEngine.Debug.Log("NewObj" + canvas.Find("NewURLObj"));
+           // UnityEngine.Debug.Log("NewImage" +canvas.Find("NewImage"));
+
+           RawImage image = (RawImage)FindObjectOfType(typeof(RawImage));
+           UnityEngine.Debug.Log("NewImage" +image);
+
+
+
+           // while (image){
+           //  Destroy(image);
+           //  yCoord=-20;
+           //  image = (RawImage)FindObjectOfType(typeof(RawImage));
+           // }
+
+           if (image){
+            UnityEngine.Debug.Log("Inside first if statement 2");
+            Destroy(image);
+            yCoord=-20;
+           }
+           
+           
            step_number += 1;
            called = false;
            Hold(1);
-           UnityEngine.Debug.Log("Inside first if statement");
+           firstUpdate=true;
+           
       } else if (GetDone()) {
            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
            SceneManager.LoadSceneAsync("Recipe Chooser");
@@ -147,10 +176,13 @@ public class StepCanvas : MonoBehaviour
            videoURL= RecipeInfo.RecipeVar.steps[step_number].videoUrl;
            URLs=URLsList[step_number]; 
            UnityEngine.Debug.Log(URLs);
+
            
       }
 
-      if (!string.IsNullOrEmpty(videoURL)){
+      // if (!string.IsNullOrEmpty(videoURL))
+
+      if (false) {
         GameObject NewObj = new GameObject(); //Create the GameObject
         RawImage Screen = NewObj.AddComponent<RawImage>(); //Add the Image Component script
         Screen.transform.SetParent(canvas.transform,false);
@@ -164,21 +196,40 @@ public class StepCanvas : MonoBehaviour
         videoPlayer.url = videoURL;
         StartCoroutine(PlayVideo(Screen));
 
+
       }
 
 
-       canvas = GameObject.Find("Canvas");
-       foreach (string currentURL in URLs)
-       {
-            GameObject NewObj = new GameObject(); //Create the GameObject
-            RawImage NewImage = NewObj.AddComponent<RawImage>(); //Add the Image Component script
-            NewImage.transform.SetParent(canvas.transform, false);
-            NewObj.GetComponent<RectTransform>().anchoredPosition = new Vector3(xCoord, yCoord, 0);
-            NewObj.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
-            yCoord = yCoord - 50;
-            NewObj.SetActive(true); //Activate the GameObject
-            StartCoroutine(GetTexture(currentURL, NewObj));
-       }
+       
+       // if (URLs!= Null){
+         foreach (string currentURL in URLs)
+         {
+              // UnityEngine.Debug.Log("each URL string1 " + currentURL);
+              // int length = currentURL.Length;
+              // string currentURLcorrected = currentURL.Substring(1, -2);
+              
+              if (firstUpdate){
+              // string currentURLcorrected = currentURL.Replace("\"","");
+              string currentURLcorrected;
+              currentURLcorrected = currentURL;
+              int length = currentURLcorrected.Length;
+              if (length !=0){
+               currentURLcorrected = currentURLcorrected.Substring(1,  length-2 );
+              UnityEngine.Debug.Log("each URL string2 " + currentURLcorrected);
+              GameObject NewURLObj = new GameObject(); //Create the GameObject
+              RawImage NewImage = NewURLObj.AddComponent<RawImage>(); //Add the Image Component script
+              NewImage.transform.SetParent(canvas.transform, false);
+              NewURLObj.GetComponent<RectTransform>().anchoredPosition = new Vector3(xCoord, yCoord, 0);
+              NewURLObj.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+              yCoord = yCoord - 50;
+              NewURLObj.SetActive(true); //Activate the GameObject
+              StartCoroutine(GetTexture(currentURLcorrected, NewURLObj));
+            }
+            }
+
+         }
+         firstUpdate=false;
+        // }
        
        
        //time each step here
