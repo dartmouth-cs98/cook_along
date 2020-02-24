a4d28cdf2e3fa751c8df6de399264f2e383c33a0
Zack Johnson <zacharyjohnsonri@gmail.com>
2020-02-23 20:12:20 -0500

    button section of tutorial

--- Assets/Scripts/TutorialLanding.cs
+++ Assets/Scripts/TutorialLanding.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using MagicLeapTools;
 using UnityEngine;
@@ -8,21 +9,71 @@ public class TutorialLanding : MonoBehaviour
 {
     
     public ControlInput controlInput;
+    public Text instructionText;
+    private int _textIndex;
+    private List<string> _instructions;
 
     private void Awake()
     {
         controlInput.OnTriggerPressEnded.AddListener(HandleTrigger);
         controlInput.OnHomeButtonTap.AddListener(HandleHomeTap);
+        controlInput.OnDoubleTap.AddListener(HandleDoubleTap);
+        controlInput.OnBumperHold.AddListener(HandleBumperHold);
+        _textIndex = 0;
+        _instructions = new List<string>()
+        {
+            "The trigger selects an item. Pull the trigger.",
+            "The home button moves you back a screen. Hit the home button",
+            "Holding the bumper repositions the screen. The screen will be placed on release. Hold the bumper.",
+            "Great job! Pull the trigger to move to the next phase of the tutorial. Press the home button to start this section again from the beginning."
+        };
+    }
+
+    void HandleDoubleTap(Vector4 param)
+    {
+        Loader.Load(Loader.Scene.WelcomeScreen);
     }
 
     void HandleTrigger()
     {
-        Loader.Load(Loader.Scene.RecipeChooser);
+        if (_textIndex == 0)
+        {
+            UpdateInstructions();
+        }
+
+        if (_textIndex == 3)
+        {
+            Loader.Load(Loader.Scene.TutorialGestures);
+        }
+        
     }
 
     void HandleHomeTap()
     {
-        Loader.Load(Loader.Scene.WelcomeScreen);
+        if (_textIndex == 1)
+        {
+            UpdateInstructions();
+        }
+
+        if (_textIndex == 3)
+        {
+            _textIndex = -1;
+            UpdateInstructions();
+        }
+    }
+    
+    void HandleBumperHold()
+    {
+        if (_textIndex == 2)
+        {
+            UpdateInstructions();
+        }
+    }
+    
+    void UpdateInstructions()
+    {
+        _textIndex += 1;
+        instructionText.text = _instructions[_textIndex];
     }
 
     // Update is called once per frame
