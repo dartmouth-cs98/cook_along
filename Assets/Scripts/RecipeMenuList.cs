e2d68598d31ec8deb24e42007c6ec267cef3b722
Zack Johnson <zacharyjohnsonri@gmail.com>
2020-02-29 15:59:35 -0500

    just saving work

--- Assets/Scripts/RecipeMenuList.cs
+++ Assets/Scripts/RecipeMenuList.cs
@@ -1,16 +1,65 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using MagicLeapTools;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
 
 public class RecipeMenuList : MonoBehaviour
-{ 
-    [SerializeField] 
+{
+    [SerializeField] public ControlInput controlInput;
     private GameObject cardTemplate;
-
+    private int _activeIndex;
     private List<int> intList;
+    private List<Recipe> _recipes;
+    private List<GameObject> _cards;
+
+    private void Awake()
+    {
+        controlInput.OnTriggerPressEnded.AddListener(HandleTrigger);
+        controlInput.OnSwipe.AddListener(HandleSwipe);
+        _activeIndex = 0;
+        _cards = new List<GameObject>();
+    }
+
+    void HandleTrigger()
+    {
+        Debug.Log("Trigger pressed");
+    }
+
+    void HandleSwipe(MLInputControllerTouchpadGestureDirection direction)
+    {
+        if (direction == MLInputControllerTouchpadGestureDirection.Up && _activeIndex > 0)
+        {
+            UpdateActiveRecipe(direction);
+        }
+
+        if (direction == MLInputControllerTouchpadGestureDirection.Down && _activeIndex < _recipes.Count - 1)
+        {
+            UpdateActiveRecipe(direction);
+        }
+    }
+    
+    void UpdateActiveRecipe(MLInputControllerTouchpadGestureDirection direction)
+    {
+        GameObject previousCard = _cards[_activeIndex];
+        
+        if (direction == MLInputControllerTouchpadGestureDirection.Up)
+        {
+            _activeIndex -= 1;
+        }
+
+        if (direction == MLInputControllerTouchpadGestureDirection.Down)
+        {
+            _activeIndex += 1;
+        }
+        
+        GameObject nextCard = _cards[_activeIndex];
+        previousCard.GetComponent<Image>().color = Color.white;
+        nextCard.GetComponent	<Image>().color = Color.yellow;
+    }  
 
     void Start()
     {
@@ -19,12 +68,16 @@ public class RecipeMenuList : MonoBehaviour
 
     void PopulateRecipes(List<Recipe> recipeList)
     {
-        foreach(Recipe recipe in recipeList)
+        for (int i = 0; i < recipeList.Count; i++)
         {
+            Recipe recipe = recipeList[i];
             GameObject card = Instantiate(cardTemplate) as GameObject;
             card.SetActive(true);
             card.GetComponent<RecipeMenuCard>().SetInfo(recipe);
+            Image cardImage = card.GetComponent<Image>();
+            cardImage.color = i == 0 ? Color.yellow : Color.white;
             card.transform.SetParent(cardTemplate.transform.parent, false);
+            _cards.Add(card);
         }
     }
 
@@ -45,6 +98,7 @@ public class RecipeMenuList : MonoBehaviour
                 string result = req.downloadHandler.text;
                 result = "{\"recipes\":" + result + "}";
                 RecipeArrayWrapper recipeWrapper = JsonUtility.FromJson<RecipeArrayWrapper>(result);
+                _recipes = recipeWrapper.recipes;
                 onSuccess(recipeWrapper.recipes);
             }
         }
