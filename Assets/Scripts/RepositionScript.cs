49ce0013728b1bcb47e1ae89b4986706c5aef8bc
Anders Robert Limstrom <anders.r.limstrom.20@dartmouth.edu>
2020-02-29 14:04:59 -0500

    Repositioning is now in HandleBumperHold from controlInput.OnBumperHold

--- Assets/Scripts/RepositionScript.cs
+++ Assets/Scripts/RepositionScript.cs
@@ -14,25 +14,20 @@ public class RepositionScript : MonoBehaviour
   public GameObject Camera;
 
 	private const float _distance = 2.0f;
-/*
+
 	private void Awake()
 	{
-		controlInput.OnBumperDown.AddListener(HandleBumper);
+		controlInput.OnBumperDown.AddListener(HandleBumperDown);
 	}
-	public void HandleBumper()
+	public void HandleBumperDown()
   {
 		WorldCanvas.transform.position = Camera.transform.position + Camera.transform.forward * _distance;
 		WorldCanvas.transform.rotation = Camera.transform.rotation;
 		persistentBehavior.UpdateBinding();
 	}
-*/
+
 	// Update is called once per frame
 	void Update()
 	{
-		if (controlInput.Bumper) {
-			WorldCanvas.transform.position = Camera.transform.position + Camera.transform.forward * _distance;
-			WorldCanvas.transform.rotation = Camera.transform.rotation;
-			persistentBehavior.UpdateBinding();
-		}
 	}
 }
